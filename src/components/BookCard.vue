cbbb884ca1e7c5ff4f3a922553d65022afe14984
DevTony101 <tonycortesano@gmail.com>
2020-08-10 21:11:50 -0500

    Creating vue component for the book controls on home page

---
+++ src/components/BookCard.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="card rounded">
+    <img
+      class="rounded book-cover"
+      src="../assets/demo.jpg"
+      alt="Book Cover"
+      width="170"
+      height="230"
+    />
+    <div class="book-info">
+      <h1>Harry Popotter y la Piedra Filosofal</h1>
+      <p>Cantidad Disponible: 500</p>
+      <p>Precio: 5$</p>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "BookCard",
+  };
+</script>
+
+<style scoped>
+  .rounded {
+    border-radius: 1rem;
+  }
+
+  .card {
+    margin: 0 0 30px 0;
+    display: inline-flex;
+    text-align: center;
+    background-color: var(--primary-variant);
+    padding: 1rem 1rem;
+    transition: background 500ms ease-in-out, color 1000ms ease-in-out;
+  }
+
+  .book-info {
+    margin: 0 1.5rem;
+    padding: 0 2.5rem;
+  }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,30 @@
 <template>
   <div>
-    <p>aaaaaaaaaaaa</p>
+    <section class="main-section">
+      <BookCard></BookCard>
+      <BookCard></BookCard>
+      <BookCard></BookCard>
+      <BookCard></BookCard>
+      <BookCard></BookCard>
+      <BookCard></BookCard>
+      <BookCard></BookCard>
+    </section>
   </div>
 </template>
 
-<style scoped></style>
+<script>
+  import BookCard from "../components/BookCard";
+  export default {
+    components: { BookCard },
+  };
+</script>
+
+<style scoped>
+  .main-section {
+    margin: 1.5rem 1rem;
+    padding: 0 1rem;
+    display: flex;
+    flex-flow: row wrap;
+    justify-content: space-between;
+  }
+</style>
