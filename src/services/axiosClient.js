21d8e096eeee58ccbc0d5cd1483a2f63a528b1a7
DevTony101 <tonycortesano@gmail.com>
2020-08-12 17:39:09 -0500

    Chaging the base url for the api calls

--- src/services/axiosClient.js
+++ src/services/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const apiClient = axios.create({
-  baseURL: "http://localhost:3000", // This is the base url that all the api calls are going to use
+  baseURL: "http://localhost:8080/api/", // This is the base url that all the api calls are going to use
   withCredentials: false,
   headers: {
     // Authentication and Configuration
