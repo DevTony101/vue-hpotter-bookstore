9d673fba11161930ee52dd80bf938cd2195c42b3
DevTony101 <tonycortesano@gmail.com>
2020-08-12 10:59:44 -0500

    Adding nprogress in router guards before and after

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import NProgress from "nprogress";
 import Cart from "../views/Cart";
 
 Vue.use(VueRouter);
@@ -23,6 +24,15 @@ const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
   routes,
+});
+
+router.beforeEach((routeTo, routeFrom, next) => {
+  NProgress.start();
+  next();
+});
+
+router.afterEach(() => {
+  NProgress.done();
 });
 
 export default router;
